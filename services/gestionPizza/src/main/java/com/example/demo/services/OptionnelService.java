9bcb992ab1d0d63efeaeb3fd4e4e52a6cbbc7cda
MonUserGit <sully.millet.29@gmail.com>
2025-03-11 14:51:10 +0100

    commmit for fix problem:

--- services/gestionPizza/src/main/java/com/example/demo/services/OptionnelService.java
+++ services/gestionPizza/src/main/java/com/example/demo/services/OptionnelService.java
@@ -1,4 +1,13 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Optionnel;
+import java.util.List;
+import java.util.Optional;
+
 public interface OptionnelService {
+    List<Optionnel> getAllOptionnels();
+    Optional<Optionnel> getOptionnelById(Long id);
+    Optionnel createOptionnel(Optionnel optionnel);
+    Optionnel updateOptionnel(Long id, Optionnel optionnel);
+    void deleteOptionnel(Long id);
 }
